56f5822fd946286008cd0d16c73602b4a356b3d8
Eric Macharia <rcmacharia7@gmail.com>
2021-11-26 09:26:15 +0300

    [FIX] update endpoint

--- product_update.py
+++ product_update.py
@@ -1,5 +1,3 @@
-# 'https://sairaj-dev2-3663518.dev.odoo.com/product_update/'
-
 import requests
 import json
 import csv
@@ -10,26 +8,19 @@ url = 'https://sairaj-dev2-3663518.dev.odoo.com/product_update/'
 headers = {'Content-Type': 'application/json'}
 file_path = '/home/kanini/update_products'
 
-with open('product.product (3).csv', encoding='utf-8') as csv_file:
-    print(csv_file)
+with open('product.csv', encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    print(csv_reader)
     start_for_loop = time.time()
     for i, row in enumerate(csv_reader):
         start_loop = time.time()
-        if i == 158:
-            print(row[1])
-            data = {"params" : { 'product': row[1]} }
-            data_json = json.dumps(data)
-            print('call url')
-            r = requests.post(url=url, data=data_json, headers=headers)
-            print(r.text)
-            print(type(r.text))
-            end_loop = time.time()
-            print(f'loop time {end_loop - start_loop}')
-            if i > 0:
-                break
+        data = {"params": {'product': row[1]}}
+        data_json = json.dumps(data)
+        print(f'call update url with data {data_json}')
+        # response = requests.post(url=url, data=data_json, headers=headers)
+        # print(response.text)
+        end_loop = time.time()
+        print(f'Single product update time {end_loop - start_loop} s')
     end_for_loop = time.time()
-    print(f'End loop {end_for_loop - start_for_loop}') 
+    print(f'Finished uploading data in {end_for_loop - start_for_loop} s')
 
 # dic = '{"jsonrpc": "2.0", "id": null, "result": {"params": {"product": "Ironman Key Chain, Part # KEY001"}}}'
--- sairaj_product_update/controllers/controllers.py
+++ sairaj_product_update/controllers/controllers.py
@@ -7,67 +7,93 @@ import json
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class SairajProductUpdate(http.Controller):
+
     @http.route('/product_update/', auth='none', type='json', method=['GET', 'POST'], cors='*', csrf=False)
     def update_product_valuation(self, **post):
         data = json.loads(request.httprequest.get_data())
         product_id = data['params']['product']
+
         if product_id:
-            _logger.info(f'product_id {product_id}')
-#             product = request.env['product.product'].sudo().search([('name', '=', data['params']['product'])])
             product = request.env['product.product'].sudo().browse(product_id)
             quants = request.env['stock.quant'].sudo().read_group(
-                [('product_id', '=', int(product_id)), ('location_id.usage', '=', 'internal'), ('quantity', '>', 0)],
+                [
+                    ('product_id', '=', int(product_id)),
+                    ('location_id.usage', '=', 'internal'),
+                    ('quantity', '>', 0)
+                ],
                 ['location_id', 'quantity'],
                 ['location_id']
             )
             _logger.info(quants)
+
             if quants:
-                _logger.info(f'categ_id valuation {product.categ_id.property_valuation}')
-                product.categ_id.write({'property_valuation': 'manual_periodic'})
-                _logger.info(f'categ_id valuation {product.categ_id.property_valuation}')
-                inventory = request.env['stock.inventory'].sudo().create({
-                    'name': product.name + ' - ' + datetime.today().strftime('%m/%d/%y'),
-                    'product_id': int(product_id),
-                    'filter': 'product',
-                    'location_id': 1
-                })
-                _logger.info(f'inventory {inventory}')
-                inventory.action_start()
-                _logger.info(f'inventory {inventory.state}')
-                inventory.action_reset_product_qty()
-                _logger.info(f'line_ids qty {inventory.line_ids.mapped("product_qty")}')
-                inventory.action_validate()
-                _logger.info(f'inventory {inventory.state}')
-                product.categ_id.write({'property_valuation': 'real_time'})
-                _logger.info(f'categ_id valuation {product.categ_id.property_valuation}')
-                inventory_reset = request.env['stock.inventory'].sudo().create({
-                    'name': product.name + ' - ' + datetime.today().strftime('%m/%d/%y'),
-                    'product_id': int(product_id),
-                    'filter': 'product',
-                    'location_id': 1
-                })
-                _logger.info(f'inventory_reset {inventory_reset}')
-                inventory_reset.action_start()
-                _logger.info(f'inventory_reset {inventory_reset.state}')
-                inventory_reset.line_ids.unlink()
-                _logger.info(f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
-                lines = []
-                for quant in quants:
-                    vals = {
-                        'product_id': int(product_id),
-                        'location_id': quant['location_id'][0],
-                        'product_uom_id': product.uom_id,
-                        'theoretical_qty': quant['quantity'],
-                        'product_qty': quant['quantity']
-                    }
-                    lines.append((0, 0, vals))
-                _logger.info(f'lines {lines}')
-                inventory_reset.sudo().write({
-                    'line_ids': lines
-                })
-                _logger.info(f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
-                inventory_reset.action_validate()
-                _logger.info(f'inventory_reset {inventory_reset.state}')
+                initial_category_state = product_id.categ_id.property_valuation
+                res = product.categ_id.write(
+                    {'property_valuation': 'manual_periodic'})
+                if res:  # we need this operation to happen if above write successedes
+                    for quant in quants:
+                        location_id = quant.get('location_id')[0]
+                        initial_quantity = quant.get('quantity')
+
+                        inventory = request.env['stock.inventory'].sudo().create({
+                            'name': product.name + ' - ' + datetime.now(),
+                            'product_id': product.id,
+                            'filter': 'product',
+                            'location_id': location_id
+                        })
+                        inventory.action_start()
+                        inventory.action_reset_product_qty()
+                        inventory.action_validate()
+
+                        # starting the reset
+                        new_res = product.categ_id.write(
+                            {'property_valuation': initial_category_state})
+
+                        if new_res:
+                            inventory_reset = request.env['stock.inventory'].sudo().create({
+                                'name': product.name + ' - ' + datetime.today().strftime('%m/%d/%y'),
+                                'product_id': product.id,
+                                'filter': 'product',
+                                'location_id': location_id
+                            })
+                            inventory_reset.action_start()
+
+                            line = inventory_reset.line_ids.filtered(
+                                lambda x: x.product_id.id == product.id)
+                            line_res = line.write({'product_qty': initial_quantity})
+
+                            if line_res:
+                                result = inventory_reset.action_validate()
+                                return {'result':result,'message': 'Product updated successfully'}
+
+        return {'result': 'Error', 'message': 'Product update failed!'}
+
+
+
+                # if res:
+                #     _logger.info(f'inventory_reset {inventory_reset.state}')
+                #     inventory_reset.line_ids.unlink()
+                #     _logger.info(
+                #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
+                #     lines = []
+                #     for quant in quants:
+                #         vals = {
+                #             'product_id': int(product_id),
+                #             'location_id': quant['location_id'][0],
+                #             'product_uom_id': product.uom_id,
+                #             'theoretical_qty': quant['quantity'],
+                #             'product_qty': quant['quantity']
+                #         }
+                #         lines.append((0, 0, vals))
+                #     _logger.info(f'lines {lines}')
+                #     inventory_reset.sudo().write({
+                #         'line_ids': lines
+                #     })
+                #     _logger.info(
+                #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
+                #     inventory_reset.action_validate()
+                #     _logger.info(f'inventory_reset {inventory_reset.state}')
 
         return product
