df6332586373fcf812b745618192a79d838a9693
Eric Macharia <rcmacharia7@gmail.com>
2021-11-26 09:34:24 +0300

    [FIX] typos and abit refactor

--- sairaj_product_update/controllers/controllers.py
+++ sairaj_product_update/controllers/controllers.py
@@ -29,16 +29,16 @@ class SairajProductUpdate(http.Controller):
             _logger.info(quants)
 
             if quants:
-                initial_category_state = product_id.categ_id.property_valuation
+                initial_category_state = product.categ_id.property_valuation
                 res = product.categ_id.write(
                     {'property_valuation': 'manual_periodic'})
-                if res:  # we need this operation to happen if above write successedes
+                if res:  # we need this operation to happen if above write returns True
                     for quant in quants:
                         location_id = quant.get('location_id')[0]
                         initial_quantity = quant.get('quantity')
 
                         inventory = request.env['stock.inventory'].sudo().create({
-                            'name': product.name + ' - ' + datetime.now(),
+                            'name': product.name + ' - ' + str(datetime.now()),
                             'product_id': product.id,
                             'filter': 'product',
                             'location_id': location_id
@@ -62,38 +62,37 @@ class SairajProductUpdate(http.Controller):
 
                             line = inventory_reset.line_ids.filtered(
                                 lambda x: x.product_id.id == product.id)
-                            line_res = line.write({'product_qty': initial_quantity})
+                            line_res = line.write(
+                                {'product_qty': initial_quantity})
 
                             if line_res:
                                 result = inventory_reset.action_validate()
-                                return {'result':result,'message': 'Product updated successfully'}
+                                return {'result': result, 'message': 'Product updated successfully'}
 
         return {'result': 'Error', 'message': 'Product update failed!'}
 
-
-
-                # if res:
-                #     _logger.info(f'inventory_reset {inventory_reset.state}')
-                #     inventory_reset.line_ids.unlink()
-                #     _logger.info(
-                #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
-                #     lines = []
-                #     for quant in quants:
-                #         vals = {
-                #             'product_id': int(product_id),
-                #             'location_id': quant['location_id'][0],
-                #             'product_uom_id': product.uom_id,
-                #             'theoretical_qty': quant['quantity'],
-                #             'product_qty': quant['quantity']
-                #         }
-                #         lines.append((0, 0, vals))
-                #     _logger.info(f'lines {lines}')
-                #     inventory_reset.sudo().write({
-                #         'line_ids': lines
-                #     })
-                #     _logger.info(
-                #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
-                #     inventory_reset.action_validate()
-                #     _logger.info(f'inventory_reset {inventory_reset.state}')
+        # if res:
+        #     _logger.info(f'inventory_reset {inventory_reset.state}')
+        #     inventory_reset.line_ids.unlink()
+        #     _logger.info(
+        #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
+        #     lines = []
+        #     for quant in quants:
+        #         vals = {
+        #             'product_id': int(product_id),
+        #             'location_id': quant['location_id'][0],
+        #             'product_uom_id': product.uom_id,
+        #             'theoretical_qty': quant['quantity'],
+        #             'product_qty': quant['quantity']
+        #         }
+        #         lines.append((0, 0, vals))
+        #     _logger.info(f'lines {lines}')
+        #     inventory_reset.sudo().write({
+        #         'line_ids': lines
+        #     })
+        #     _logger.info(
+        #         f'line_ids qty {inventory_reset.line_ids.mapped("product_qty")}')
+        #     inventory_reset.action_validate()
+        #     _logger.info(f'inventory_reset {inventory_reset.state}')
 
         return product
